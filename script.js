00e47bf707f881fdaf48857836eb1bd8c55ae7d9
daivat9228 <daivat796@gmail.com>
2025-11-14 11:50:09 +0530

    conditional statement

--- script.js
+++ script.js
@@ -1,2 +1,45 @@
 console.log("JavaScript loaded successfully!");
 // Master the basics of JavaScript.
+// Video 1: Basic logic on JS -> Variable, operator,operation on operators
+//  > Q. sum of tw integer 
+//  - relation between integer and string
+//  > Q. sum and message
+//  - type conversion
+//  > Q. Accept and print the answer
+//  > Q. Swap two variable via 3 methods
+
+// Video 2: Conditional statement
+//  > Q. Valid voter
+          // var ans = Number(prompt("Enter your age: "));
+
+          // if(isNaN(ans)){
+          //      console.log("wrong input")
+          // }
+          // else if(ans >= 18 ){
+          //      console.log("you can vote");
+          // }
+          // else{
+          //      console.log("you cannot vote");
+          // }
+
+
+//  > Q. Shop discout
+          // let amount = Number(prompt("Whats the final amount?"))
+          // let dis = 0
+
+          // if(amount > 0 && amount <= 5000){
+          //      console.log(amount)
+          // }else if (amount > 5000 && amount <= 7000){
+          //      dis=5
+          // }else if (amount > 7000 && amount <= 9000){
+          //      dis=10
+          // }else if (amount > 9000){
+          //      dis=20
+          // }else {
+          //      console.log("wrong input");
+          // }
+          // console.log(amount - Math.floor((dis*amount)/100));
+
+
+//  > Q. Bijli bill
+
