7ca7ff07766e8d34c8fef7028da39a2965ea10e1
daivat9228 <daivat796@gmail.com>
2025-11-15 14:11:04 +0530

    switch cases

--- script.js
+++ script.js
@@ -62,5 +62,101 @@ console.log("JavaScript loaded successfully!");
           // console.log(amount);
 
 
+//  > Q. INR denomination
+
+          // let amount = Number(prompt("Enter amount in INR: ")) // 7862
+          // if(amount >= 500){
+          //      let note500 = Math.floor(amount / 500);
+          //      amount = amount % 500;
+          //      console.log("500 INR notes: " + note500);
+          // }
+          // if(amount >= 200){
+          //      let note200 = Math.floor(amount / 200);
+          //      amount = amount % 200;
+          //      console.log("200 INR notes: " + note200);
+          // }
+          // if(amount >= 100){
+          //      let note100 = Math.floor(amount / 100);
+          //      amount = amount % 100;
+          //      console.log("100 INR notes: " + note100);
+          // }
+          // if(amount >= 50){
+          //      let note50 = Math.floor(amount / 50);
+          //      amount = amount % 50;
+          //      console.log("50 INR notes: " + note50);
+          // }
+          // if(amount >= 20){
+          //      let note20 = Math.floor(amount / 20);
+          //      amount = amount % 20;    
+          //      console.log("20 INR notes: " + note20);
+          // }
+          // if(amount >= 10){
+          //      let note10 = Math.floor(amount / 10);
+          //      amount = amount % 10;    
+          //      console.log("10 INR notes: " + note10);
+          // }
+          // if(amount >= 5){
+          //      let note5 = Math.floor(amount / 5);
+          //      amount = amount % 5;    
+          //      console.log("5 INR notes: " + note5);
+          // }
+          // if(amount >= 2){
+          //      let note2 = Math.floor(amount / 2);
+          //      amount = amount % 2;    
+          //      console.log("2 INR notes: " + note2);
+          // }
+          // if(amount >= 1){
+          //      let note1 = Math.floor(amount / 1);
+          //      amount = amount % 1;    
+          //      console.log("1 INR notes: " + note1);
+          // }
+
+     // ------------------- Switch case ------------------
+          // let day = Number(prompt("Enter day number (1-7): "));
+
+          // switch(day){   // for contant value checking, switch case is used
+          //      case 1:console.log("monday");
+          //      break;    
+          //      case 2:console.log("tuesday");     
+          //      break;
+          //      case 3:console.log("wednesday");     
+          //      break;
+          //      case 4:console.log("thursday");    
+          //      break;
+          //      case 5:console.log("friday");     
+          //      break;
+          //      case 6:console.log("saturday");     
+          //      break;
+          //      case 7:console.log("sunday");     
+          //      break;
+          //      default:console.log("invalid input");
+          // }
+
+          // let day = Number(prompt("Enter day number (1-7): "));
+
+          // switch(day){
+          //      case 1:   
+          //      case 2:
+          //      case 3:console.log("working day");
+          //      break;    
+          //      case 4:   
+          //      case 5:
+          //      case 6:
+          //      case 7:console.log("holiday");
+          //      break;
+          //      default:console.log("invalid input");
+          // }
+
+          // let num = Number(prompt("Enter any number: "));
+          // switch(true){
+          //      case num > 0:console.log("positive number");
+          //      break;
+          //      case num < 0:console.log("negative number");
+          //      break;
+          //      case num === 0:console.log("zero");
+          //      break;
+          //      default:console.log("invalid input");
+          // }
 
 
+// Video 3: Everything about loops
